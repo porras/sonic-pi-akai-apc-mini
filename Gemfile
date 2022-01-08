f34ea6400d963f4069bfb2f4f6d0f10b58e634f6
Sergio Gil <sgilperez@gmail.com>
2022-01-08 11:20:49 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in sonic-pi-akai-apc-mini.gemspec
+gemspec
+
+gem "rake"
+gem "rspec"
---
+++ spec/sonic-pi-akai-apc-mini_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe 'sonic-pi-akai-apc-mini' do
+  xit "has at least unit tests for calculation helper methods (or ideally for more than that)" 
+end
